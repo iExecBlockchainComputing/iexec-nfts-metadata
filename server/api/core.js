ceaa3bcf9600b6edd95bb6ec707ec6870698e25d
Hadrien Croubois <hc@iex.ec>
2020-12-02 10:59:58 +0100

    Support for sidechains + config format change

--- server/api/core.js
+++ server/api/core.js
@@ -2,34 +2,42 @@
 
 const ethers = require('ethers');
 const https  = require('https');
+const url    = require('url');
 
 exports.submit = (query, formater, config) => (req, res) => {
 	const chain   = config.chains[req.params.chainid];
 	const tokenid = ethers.utils.hexlify(ethers.utils.padZeros(ethers.utils.bigNumberify(req.params.tokenid), 20));
-
 	new Promise(async (resolve, reject) => {
-		let chunks = [];
-		let request = https.request({
-			path: chain.subgraph,
-			...config.request
-		}, res => {
-			res.on('data', (chunk) => {
-				chunks.push(chunk);
+		if (chain)
+		{
+			let chunks = [];
+			let { protocol, host, path, port } = url.parse(chain.subgraph);
+			port |= protocol == 'https:' ? 443 : 80;
+
+
+			let request = https.request({ method: "POST", host, path, port }, res => {
+				res.on('data', (chunk) => {
+					chunks.push(chunk);
+				});
+				res.on('end', () => {
+					if (chunks.length)
+					{
+						resolve(JSON.parse(chunks.join('')));
+					}
+					else
+					{
+						reject({ error: `[HTTP ERROR]\nstatusCode: ${res.statusCode}` });
+					}
+				});
 			});
-			res.on('end', () => {
-				if (chunks.length)
-				{
-					resolve(JSON.parse(chunks.join('')));
-				}
-				else
-				{
-					reject(`[HTTP ERROR]\nstatusCode: ${res.statusCode}`);
-				}
-			});
-		});
-		request.on('error', reject);
-		request.write(JSON.stringify({ query, variables: { tokenid } }));
-		request.end();
+			request.on('error', reject);
+			request.write(JSON.stringify({ query, variables: { tokenid } }));
+			request.end();
+		}
+		else
+		{
+			reject({ error: `unsupported chainid ${req.params.chainid}` });
+		}
 	})
 	.then(response => {
 		res.json(formater(response, chain, tokenid));
